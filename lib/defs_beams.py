f2a9c3284cee9460394eab93c299c330eb99e380
Masaya-Sakamoto <masaya-sakamoto@kke.co.jp>
2025-08-12 15:50:00 +0900

    Refactor def_lin_beams to include include_end parameter and update related tests for consistency

--- lib/defs_beams.py
+++ lib/defs_beams.py
@@ -1,17 +1,7 @@
-def def_lin_beams(id_start:int, theta_start:int, theta_end:int, phi_const:int, step:int=1) -> list[dict[int, int, int]]:
+def def_lin_beams(id_start:int, theta_start:int, theta_end:int, phi_const:int, include_end:bool, step:int=1) -> list[dict[int, int, int]]:
     """
     Generate a list of beam IDs based on a linear sequence.
 
     """
-    return [{"id": id_start + i, "theta": theta, "phi": phi_const} for i, theta in enumerate(range(theta_start, theta_end + 1, step))]
-
-
-def def_basic_lin_beams(theta_max:int=25, phi_0:int=0, step:int=1) -> list[dict[int, int, int]]:
-    """
-    Generate a list of basic linear beams.
-
-    """
-    origin = {"id": 1, "theta": 0, "phi": phi_0}
-    beams1 = def_lin_beams(id_start=2, theta_start=1, theta_end=theta_max, phi_const=phi_0, step=step)
-    beams2 = def_lin_beams(id_start=len(beams1) + 2, theta_start=1, theta_end=theta_max, phi_const=phi_0 + 180, step=step)
-    return [origin,] + beams1 + beams2
+    last_theta = theta_end+step if include_end else theta_end
+    return [{"id": id_start + i, "theta": theta, "phi": phi_const} for i, theta in enumerate(range(theta_start, last_theta, step))]
--- tests/test_defs_beams.py
+++ tests/test_defs_beams.py
@@ -1,26 +1,19 @@
-from lib.defs_beams import def_lin_beams, def_basic_lin_beams
+from lib.defs_beams import def_lin_beams
 
 
 def test_def_lin_beams():
     """
     Test the def_lin_beams function.
     """
-    beams = def_lin_beams(id_start=1, theta_start=0, theta_end=10, phi_const=45, step=2)
-    assert len(beams) == 5 + 1  # {0 .. 10} = {0, 2, 4, 6, 8, 10} with step 2 = 6 beams
+    beams = def_lin_beams(id_start=1, theta_start=0, theta_end=10, phi_const=45, include_end=True,step=2)
+    beams_size = len(beams)
+    assert beams_size == 5 + 1  # {0 .. 10} = {0, 2, 4, 6, 8, 10} with step 2 = 6 beams
     assert beams[0] == {"id": 1, "theta": 0, "phi": 45}
     assert beams[1] == {"id": 2, "theta": 2, "phi": 45}
-    assert beams[len(beams)-1] == {"id": 6, "theta": 10, "phi": 45}
-
-def test_def_basic_lin_beams():
-    """
-    Test the def_basic_lin_beams function.
-    """
-    beams = def_basic_lin_beams(theta_max=10, phi_0=90, step=1)
-    assert len(beams) == 21  # 1 origin + 10 + 10 beams
-    assert beams[0] == {"id": 1, "theta": 0, "phi": 90}
-    # Check first set of beams
-    assert beams[1] == {"id": 2, "theta": 1, "phi": 90}
-    assert beams[10] == {"id": 11, "theta": 10, "phi": 90}
-    # Check second set of beams
-    assert beams[11] == {"id": 12, "theta": 1, "phi": 270}  # Second set of beams
-    assert beams[20] == {"id": 21, "theta": 10, "phi": 270}
+    assert beams[beams_size - 1] == {"id": 6, "theta": 10, "phi": 45}
+    beams = def_lin_beams(id_start=1, theta_start=0, theta_end=10, phi_const=45, include_end=False, step=2)
+    beams_size = len(beams)
+    assert beams_size == 5  # {0 .. 10} = {0, 2, 4, 6, 8} with step 2 = 5 beams
+    assert beams[0] == {"id": 1, "theta": 0, "phi": 45}
+    assert beams[1] == {"id": 2, "theta": 2, "phi": 45}
+    assert beams[beams_size - 1] == {"id": 5, "theta": 8, "phi": 45}

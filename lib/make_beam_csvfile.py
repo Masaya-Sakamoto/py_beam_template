6631b6ba013dc9f1172562f432a6198e11738010
Masaya-Sakamoto <masaya-sakamoto@kke.co.jp>
2025-08-12 17:18:33 +0900

    Implement create_beam_str function and update create_beam_table to generate beam entries

--- lib/make_beam_csvfile.py
+++ lib/make_beam_csvfile.py
@@ -1,5 +1,10 @@
 from lib.defs_beams import def_lin_beams
 
+def create_beam_str(beam_entry: dict[str, int], beam_type:str):
+    if not (beam_type == 'TX' or beam_type == 'RX'):
+        raise ValueError("beam_type must be either 'TX' or 'RX'")
+    return f"{beam_type},{beam_entry['id']},0,0,{beam_entry['theta']},{beam_entry['phi']},,,,"
+
 def create_beam_table(beams:list[dict[str, int]]) -> list[dict[str,str]]:
     """
     [
@@ -14,7 +19,16 @@ def create_beam_table(beams:list[dict[str, int]]) -> list[dict[str,str]]:
       }
     ]
     """
-    pass
+    beam_table = []
+    for be in beams:
+        entry_list = []
+        entry_list.append(create_beam_str(be, 'TX'))
+        entry_list.append(create_beam_str(be, 'RX'))
+        beam_table.append({
+            'TX': entry_list[0],
+            'RX': entry_list[1]
+        })
+    return beam_table
 
 def create_beam_table_csv(beam_table_str_lists:list[dict[str,str]], filename:str):
     pass

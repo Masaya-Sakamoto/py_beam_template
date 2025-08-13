ee1eaf5214d2166cd990efdb0f339e9a78958ee6
Masaya-Sakamoto <masaya-sakamoto@kke.co.jp>
2025-08-13 16:05:27 +0900

    Implement oai_xapp_beam_management_handler and sequence_ops functions for beam management

--- lib/defs_beamsweep_op.py
+++ lib/defs_beamsweep_op.py
@@ -1,5 +1,34 @@
-def basic_sequence(beam_sweep_table: list[dict[str, int]]):
+import subprocess
+import time
+
+def oai_xapp_beam_management_handler(txBeamId:int, rxBeamId:int, program_path:str) -> bool:
     """
-    逐次、xappを実行する
     
+    This function just calls the oai_xapp_beam_management() function from the OAI xApp.
+    returns True if the function executes successfully, otherwise returns False.
+    Usage: ./oaibox_xapp_beam_management --txBeamId <int> --rxBeamId <int>
     """
+    # oai_xapp_beam_management() takes txBeamId and rxBeamId as arguments
+    try:
+        result = subprocess.run([program_path, "--txBeamId", str(txBeamId), "--rxBeamId", str(rxBeamId)])
+        return result.returncode == 0
+    except subprocess.CalledProcessError as e:
+        print(f"Command failed: {e.stderr}")
+        return False
+    except FileNotFoundError:
+        print("Command not found")
+        return False
+
+def sequence_ops(beam_sweep_tables: list[list[dict[str, int]]], program_path:str, interval:int=5) -> bool:
+    """
+    beam_sweep_table: beam sweep sequenceテーブル {"id", "theta", "phi"}
+    oai_xapp_beam_management_handlerをbeam_sweep_tableに基づいて実行する.
+    """
+    for beam_sweep_table in beam_sweep_tables:
+        for beam in beam_sweep_table:
+            txBeamId = beam["id"]
+            rxBeamId = beam["id"]
+            if not oai_xapp_beam_management_handler(txBeamId, rxBeamId, program_path):
+                return False
+            time.sleep(interval)
+    return True
---
+++ tests/test_defs_beamsweep_op.py
@@ -0,0 +1,33 @@
+from lib.defs_beams import def_lin_beams
+from lib.defs_beam_sweeping import def_basic_lin_beamsweeping
+from lib.defs_beamsweep_op import sequence_ops
+
+retcode = 0
+def init_testcase(testcase_retcode:int):
+    global retcode
+    retcode = testcase_retcode
+
+def mock_oaibox_xapp_beam_management_handler(txBeamId:int, rxBeamId:int, program_path:str) -> bool:
+    return retcode == 0
+
+def test_sequence_ops(mocker):
+    program_path = "/path/to/oai_xapp_beam_management" # dummy
+
+    # beam sweep sequenceを定義
+    origin = {"id": 1, "theta": 0, "phi": 0}
+    beam_sweep_tables = [
+        [
+            origin,
+            {"id": 2, "theta": 10, "phi": 0},
+            origin
+        ]
+    ]
+
+    # mock initialize: subprocess.run, sleep
+    mocker.patch('time.sleep')
+    mocker.patch('lib.defs_beamsweep_op.oai_xapp_beam_management_handler', new=mock_oaibox_xapp_beam_management_handler)
+
+    init_testcase(0) # Mock successful execution
+    assert sequence_ops(beam_sweep_tables, program_path) == True
+    init_testcase(1) # Mock failed execution
+    assert sequence_ops(beam_sweep_tables, program_path) == False

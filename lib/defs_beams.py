a38921829643a8f878c26f216ee19941581e35f0
Masaya-Sakamoto <masaya-sakamoto@kke.co.jp>
2025-08-20 15:27:42 +0900

    Add phyllotaxis functions for beam generation and mapping

--- lib/defs_beams.py
+++ lib/defs_beams.py
@@ -1,3 +1,5 @@
+import math
+
 def def_lin_beams(id_start:int, theta_start:int, theta_end:int, phi_const:int, include_end:bool, step:int=1) -> list[dict[str, int]]:
     """
     Generate a list of beam IDs based on a linear sequence.
@@ -5,3 +7,65 @@ def def_lin_beams(id_start:int, theta_start:int, theta_end:int, phi_const:int, i
     """
     last_theta = theta_end+step if include_end else theta_end
     return [{"id": id_start + i, "theta": theta, "phi": phi_const} for i, theta in enumerate(range(theta_start, last_theta, step))]
+
+def __snap_integer_angle(float_angle:float) -> int:
+    return int(round(float_angle * 180 / math.pi, 0) * math.pi / 180)
+
+def __golden_angle():
+    phi = (1 + math.sqrt(5)) / 2
+    return 2 * math.pi / (phi**2)
+
+def __phyllotaxis_points(N:int, delta:float) -> tuple[map[float], map[float]]:
+    """
+    平面上のphyllotaxis点列を (r, theta) で返す。
+    半径は単位円内に正規化。
+    delta \\in [0,1)
+    """
+    n = range(N)
+    ga = __golden_angle()
+    r = map(lambda n: math.sqrt((n + delta) / N), n)
+    phi = map(lambda n: n * ga, n)
+    return r, phi
+
+def __map_to_zenith_angle(r:map[float], phi:map[float]) -> map[float]:
+    theta = map(lambda r: math.acos(1 - r**2), r)
+    return theta
+
+def __mod_theta_phi(
+        theta:map[float],
+        phi:map[float],
+        theta_max:float|None=None,
+        center_theta:float|None=None,
+        center_phi:float|None=None,
+        is_int_val:bool|None=False,
+    ) -> tuple[map[float], map[float]]:
+    if theta_max:
+        theta = map(lambda t: t * theta_max * 2 / math.pi, theta)
+    if center_theta:
+        print("Warning: center_theta is not implemented in __mod_theta_phi")
+    if center_phi:
+        print("Warning: center_phi is not implemented in __mod_theta_phi")
+    if is_int_val is not None and is_int_val==True:
+        theta = map(__snap_integer_angle, theta)
+        phi = map(__snap_integer_angle, phi)
+        # extract unique
+        # not implemented yet
+    return (theta, phi)
+
+def __phyllotaxis_point_on_hemisphere(N:int, delta:float, theta_max:float, is_int_val:bool) -> tuple[map[float], map[float]]:
+    """
+    1. phyllotaxis点を単位円盤に生成する ... __phyllotaxis_points
+    2. それを当面積写像で半球にマッピングする
+    3. 最後に天頂角を制限して返す -- 2., 3. ... __map_to_zenith_angle
+    """
+    # 極座標系でphyllotaxis点列を生成
+    r, phi = __phyllotaxis_points(N, delta)
+    # 天頂角にマッピング
+    theta = __map_to_zenith_angle(r, phi)
+    # 天頂角を制限して返す
+    theta, phi = __mod_theta_phi(theta, phi, theta_max=theta_max, is_int_val=is_int_val)
+    return (theta, phi)
+
+def def_basic_fibonacci_beams(N:int, delta:float, theta_max:float, is_int_val:bool) -> tuple[list[float|int], list[float|int]]:
+    theta, phi = __phyllotaxis_point_on_hemisphere(N, delta, theta_max, is_int_val)
+    return (list(theta), list(phi))

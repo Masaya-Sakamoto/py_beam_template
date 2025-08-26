0bb678c23d7c2b50de8a36858d71e00a25f90369
Masaya-Sakamoto <masaya-sakamoto@kke.co.jp>
2025-08-26 19:01:09 +0900

    beam_control_tableランダム遷移の場合でも数だけは確認できた。

--- tests/test_create_beam_control_table.py
+++ tests/test_create_beam_control_table.py
@@ -129,3 +129,128 @@ def test_create_lin_seq_beam_control_table():
         'phi_d': 0,
         'duration': 5,
     }
+
+def test_create_fib_rng_beam_control_table():
+    config = arg_parser("tests/conf/fib_rnd/fib_rnd_conf.json")
+    beam_template_lst = get_beam_template_from_json(config)
+    beam_control_program_lst = get_beam_control_program_from_json(config)
+    # beam table作成
+    beam_table = []
+    for beam_template in beam_template_lst:
+        assert beam_template['type'] == BeamPattern.FIBONACCI
+        tmp_bt = def_const_fibonacci_beams(
+                beam_template['steps'],
+                beam_template['start_point'],
+                beam_template['end_point'],
+                0,
+                config['theta_max'],
+                config['pattern_rotation_angle'],
+                config['center_angle_theta'],
+                config['center_angle_phi'],
+                True
+            )
+        beam_table += tmp_bt
+    beam_table = reset_beam_table_id(beam_table)
+
+    # control table作成
+    beam_control_table = create_beam_control_table(
+        beam_table,
+        beam_control_program_lst,
+        seed=config['random_seed']
+    )
+
+    # check
+    assert len(beam_control_table) == 196
+    assert beam_control_table[0] == {
+        'id': 1,
+        'dB': 0,
+        'theta_d': 0,
+        'phi_d': 0,
+        'duration': 10,
+    }
+    assert beam_control_table[1] == {
+        'id': 1,
+        'dB': 0,
+        'theta_d': 0,
+        'phi_d': 0,
+        'duration': 10,
+    }
+
+    assert beam_control_table[2] == {
+        'id': 62,
+        'dB': 0,
+        'theta_d': 24,
+        'phi_d': 108,
+        'duration': 5,
+    }
+
+def test_create_lin_rng_beam_control_table():
+    config = arg_parser("tests/conf/lin_rnd/lin_rnd_conf.json")
+    beam_template_lst = get_beam_template_from_json(config)
+    beam_control_program_lst = get_beam_control_program_from_json(config)
+    # beam table作成
+    beam_table = []
+    for beam_template in beam_template_lst:
+        assert beam_template['type'] == BeamPattern.CONST or beam_template['type'] == BeamPattern.LINEAR
+        if beam_template['type'] == BeamPattern.CONST:
+            tmp_bt = def_const_constant_beam(
+                1,
+                beam_template['start_point'],
+                beam_template['end_point'],
+                0,
+                config['theta_max'],
+                config['pattern_rotation_angle'],
+                config['center_angle_theta'],
+                config['center_angle_phi'],
+                True
+            )
+            beam_table += tmp_bt
+        elif beam_template['type'] == BeamPattern.LINEAR:
+            tmp_bt = def_const_linear_beams(
+                beam_template['steps'],
+                beam_template['start_point'],
+                beam_template['end_point'],
+                0,
+                config['theta_max'],
+                config['pattern_rotation_angle'],
+                config['center_angle_theta'],
+                config['center_angle_phi'],
+                True
+            )
+            beam_table += tmp_bt
+        
+        else:
+            pass
+    beam_table = reset_beam_table_id(beam_table)
+
+    # control table作成
+    beam_control_table = create_beam_control_table(
+        beam_table,
+        beam_control_program_lst,
+        seed=config['random_seed']
+    )
+
+    # check
+    assert len(beam_control_table) == 157
+    assert beam_control_table[0] == {
+        'id': 1,
+        'dB': 0,
+        'theta_d': 0,
+        'phi_d': 0,
+        'duration': 10,
+    }
+    assert beam_control_table[1] == {
+        'id': 1,
+        'dB': 0,
+        'theta_d': 0,
+        'phi_d': 0,
+        'duration': 10,
+    }
+
+    assert beam_control_table[2] == {
+        'id': 16,
+        'dB': 0,
+        'theta_d': 14,
+        'phi_d': 0,
+        'duration': 5,
+    }

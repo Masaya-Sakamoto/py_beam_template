6a86c1e2fcf344fb26493e54697deb84d5cb3dec
Masaya-Sakamoto <masaya-sakamoto@kke.co.jp>
2025-08-26 19:18:08 +0900

    Refactor test_sequence_ops to create beam control table and validate sequence operations

--- tests/test_defs_beamsweep_op.py
+++ tests/test_defs_beamsweep_op.py
@@ -1,33 +1,55 @@
-# from lib.defs_beams import def_lin_beams
-# from lib.defs_beam_sweeping import def_basic_lin_beamsweeping
-# from lib.defs_beam_sweep_op import sequence_ops
+from tools.parser import arg_parser, get_beam_template_from_json, get_beam_control_program_from_json
+from lib.defs_beams import def_const_fibonacci_beams, reset_beam_table_id
+from lib.defs_beam_sweeping import create_beam_control_table
+from pytypes.type_beam import BeamPattern
+from lib.defs_beam_sweep_op import sequence_ops
+from pytypes.type_beam import beam_sweeping_t
 
-# retcode = 0
-# def init_testcase(testcase_retcode:int):
-#     global retcode
-#     retcode = testcase_retcode
+ret_code = 0
+def init_testcase(testcase_returncode:int):
+    global ret_code
+    ret_code = testcase_returncode
 
-# def mock_oaibox_xapp_beam_management_handler(txBeamId:int, rxBeamId:int, program_path:str) -> bool:
-#     return retcode == 0
+def mock_oaibox_xapp_beam_management_handler(txBeamId:int, rxBeamId:int, program_path:str) -> bool:
+    return ret_code == 0
 
-# def test_sequence_ops(mocker):
-#     program_path = "/path/to/oai_xapp_beam_management" # dummy
+def test_sequence_ops(mocker):
+    program_path = "/path/to/oai_xapp_beam_management" # dummy
 
-#     # beam sweep sequenceを定義
-#     origin = {"id": 1, "theta": 0, "phi": 0}
-#     beam_sweep_tables = [
-#         [
-#             origin,
-#             {"id": 2, "theta": 10, "phi": 0},
-#             origin
-#         ]
-#     ]
+    # beam control table作成
+    config = arg_parser("tests/conf/fib_rnd/fib_rnd_conf.json")
+    beam_template_lst = get_beam_template_from_json(config)
+    beam_control_program_lst = get_beam_control_program_from_json(config)
+    # beam table作成
+    beam_table = []
+    for beam_template in beam_template_lst:
+        assert beam_template['type'] == BeamPattern.FIBONACCI
+        tmp_bt = def_const_fibonacci_beams(
+                beam_template['steps'],
+                beam_template['start_point'],
+                beam_template['end_point'],
+                0,
+                config['theta_max'],
+                config['pattern_rotation_angle'],
+                config['center_angle_theta'],
+                config['center_angle_phi'],
+                True
+            )
+        beam_table += tmp_bt
+    beam_table = reset_beam_table_id(beam_table)
 
-#     # mock initialize: subprocess.run, sleep
-#     mocker.patch('time.sleep')
-#     mocker.patch('lib.defs_beamsweep_op.oai_xapp_beam_management_handler', new=mock_oaibox_xapp_beam_management_handler)
+    # control table作成
+    beam_control_table = create_beam_control_table(
+        beam_table,
+        beam_control_program_lst,
+        seed=config['random_seed']
+    )
 
-#     init_testcase(0) # Mock successful execution
-#     assert sequence_ops(beam_sweep_tables, program_path) == True
-#     init_testcase(1) # Mock failed execution
-#     assert sequence_ops(beam_sweep_tables, program_path) == False
+    # mock initialize: subprocess.run, sleep
+    mocker.patch('time.sleep')
+    mocker.patch('lib.defs_beam_sweep_op.oai_xapp_beam_management_handler', new=mock_oaibox_xapp_beam_management_handler)
+
+    init_testcase(0) # Mock successful execution
+    assert sequence_ops(beam_control_table, program_path) == True
+    init_testcase(1) # Mock failed execution
+    assert sequence_ops(beam_control_table, program_path) == False
